3945725df083a9de7afa644ce184ed72cd7ecd85
SAKADA26 <74977701+SAKADANISSAY@users.noreply.github.com>
2026-02-04 08:36:50 +0700

    Update QR code URL in generate_qr.py

--- QR/qr/generate_qr.py
+++ QR/qr/generate_qr.py
@@ -1,8 +1,9 @@
 import qrcode
 
-url = "https://sakadanissay.github.io/app-download-qr/"
+url = "https://sakadanissay.github.io/chiefbank/"
 
 qr = qrcode.make(url)
 qr.save("chief_mobile_bank_qr.png")
 
 print("ONE QR with Android + iOS links created")
+
