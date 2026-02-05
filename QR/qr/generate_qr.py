97c3268f465b0272105c890d929eb04112d23ff6
SAKADA26 <74977701+SAKADANISSAY@users.noreply.github.com>
2026-02-05 09:32:05 +0700

    updatecode

--- QR/qr/generate_qr.py
+++ QR/qr/generate_qr.py
@@ -1,9 +1,10 @@
 import qrcode
 
-url = "https://chiefbankcspp-app.github.io/chiefbankapp/"
+url = "https://chiefbankcspp-app.github.io/chiefbankapp/QR"
 
 qr = qrcode.make(url)
 qr.save("chief_mobile_bank_qr.png")
 
 print("ONE QR with Android + iOS links created")
+
 
